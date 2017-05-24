53d4b81738a00e3dd25ef268e067940e752c6d8b
Luís Portela Afonso <luis.portela@mindera.com>
2017-05-25 00:09:06 +0100

    💣  Create project with swift🍛

---
+++ ReactiveFacebook.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+  s.name         = "ReactiveFacebook"
+  s.version      = "0.1"
+  s.summary      = ""
+  s.description  = <<-DESC
+    Your description here.
+  DESC
+  s.homepage     = "git@github.com:YouClap/ReactiveFacebook"
+  s.license      = { :type => "MIT", :file => "LICENSE" }
+  s.author             = { "Luís Portela" => "me@portellaa.eu" }
+  s.social_media_url   = ""
+  s.ios.deployment_target = "8.0"
+  s.source       = { :git => "git@github.com:YouClap/ReactiveFacebook.git", :tag => s.version.to_s }
+  s.source_files  = "Sources/**/*"
+  s.frameworks  = "Foundation"
+end
---
+++ Tests/ReactiveFacebookTests/ReactiveFacebookTests.swift
@@ -0,0 +1,23 @@
+//
+//  ReactiveFacebookTests.swift
+//  ClapWare
+//
+//  Created by Luís Portela on {TODAY}.
+//  Copyright © 2017 ClapWare. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import ReactiveFacebook
+
+class ReactiveFacebookTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(ReactiveFacebook().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
